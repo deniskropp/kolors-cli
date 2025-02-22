8220d5081ad3cca2cf54b7e9155366f1eaa23803
Denise Schleier <denise.schleier@outlook.com>
2025-02-22 23:04:12 +0100

    v1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='kolors',
+    name='kolors-cli',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
@@ -11,10 +11,23 @@ setup(
         'torch==2.2.0',
         'transformers==4.42.4',
         'sentencepiece==0.1.99',
+        'gradio==4.38.1',
+        'huggingface_hub==0.16.4',
+        'Pillow==10.0.0',
     ],
     entry_points={
         'console_scripts': [
-            'kolors=kolors.app:Kolors.launch'
+            'kolors-cli=kolors.kolors_cli:main',
         ],
     },
+    author='Denis Kropp',
+    author_email='dok@directfb1.org',
+    description='A CLI tool for generating images based on prompts using Kwai-Kolors.',
+    url='https://github.com/deniskropp/kolors-cli',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
 )
